21279713dfcf569d2ab414c056f8bb0c0c88c091
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2019-07-29 16:42:43 +0300

    Call Setup in JMH for each invocation

--- src/jmh/kotlin/quantum/datatype/IteratorTest.kt
+++ src/jmh/kotlin/quantum/datatype/IteratorTest.kt
@@ -1,48 +1,54 @@
 package quantum.datatype
 
 import org.openjdk.jmh.annotations.*
+import java.util.concurrent.ThreadLocalRandom
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 15, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 @State(Scope.Benchmark)
 open class IteratorTest {
 
-    @Param("1", "2", "3")
+    @Param("100", "200", "300")
     var value = ""
 
-    private var vector: Array<Double> = arrayOf()
-    private var iterator = ArrayIterator<Double>()
-    private var indexedValueIterator = ArrayIndexedValueIterator(0.0)
+    private val zero = Pair(0.0, 0.0)
+    private var vector: Array<Pair<Double, Double>> = arrayOf()
+    private var iterator = ArrayIterator<Pair<Double, Double>>()
+    private var indexedValueIterator = ArrayIndexedValueIterator(zero)
 
-    @Setup
+
+    @Setup(Level.Invocation)
     fun setUp() {
         val size = value.toInt()
-        vector = Array(size) { Random.nextDouble() }
+        val rnd = ThreadLocalRandom.current()
+        vector = Array(size) { Pair(rnd.nextDouble(), rnd.nextDouble()) }
         iterator = ArrayIterator(*vector)
-        indexedValueIterator = ArrayIndexedValueIterator(0.0, *vector)
+        indexedValueIterator = ArrayIndexedValueIterator(zero, *vector)
     }
+
+    fun getValue(pair: Pair<Double, Double>): Double = pair.first + pair.second
 
     @Benchmark
     fun testRangeLoop(): Double {
-        var sum = 0.0;
+        var sum = 0.0
 
         for (i in 0 until vector.size) {
-            sum += vector[i]
+            sum += getValue(vector[i])
         }
         return sum
     }
 
     @Benchmark
     fun testIterator(): Double {
-        var sum = 0.0;
+        var sum = 0.0
 
         for (value in iterator) {
-            sum += value
+            sum += getValue(value)
         }
 
         return sum
@@ -50,11 +56,11 @@ open class IteratorTest {
 
     @Benchmark
     fun testIndexedValueIterator(): Double {
-        var sum = 0.0;
+        var sum = 0.0
 
         while (indexedValueIterator.hasNext()) {
-            indexedValueIterator.next { _, d ->
-                sum += d
+            indexedValueIterator.next { _, value ->
+                sum += getValue(value)
             }
         }
 
