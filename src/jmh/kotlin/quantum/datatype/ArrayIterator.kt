28fc15695e1d1f82a388e1f24df0d98894d75842
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2019-07-30 15:02:30 +0300

    Make iterators use indices in JMH test

--- src/jmh/kotlin/quantum/datatype/ArrayIterator.kt
+++
@@ -1,9 +0,0 @@
-package quantum.datatype
-
-class ArrayIterator<T>(vararg val values: T) : Iterator<T> {
-
-    var index = 0
-
-    override fun hasNext() = index < values.size
-    override fun next() = values[index++]
-}
--- src/jmh/kotlin/quantum/datatype/IndexedValueIteratorBenchmark.kt
+++ src/jmh/kotlin/quantum/datatype/IndexedValueIteratorBenchmark.kt
@@ -3,7 +3,6 @@ package quantum.datatype
 import org.openjdk.jmh.annotations.*
 import java.util.concurrent.ThreadLocalRandom
 import java.util.concurrent.TimeUnit
-import kotlin.random.Random
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -11,34 +10,33 @@ import kotlin.random.Random
 @Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 @State(Scope.Benchmark)
-open class IteratorTest {
+open class IndexedValueIteratorBenchmark {
 
     @Param("100", "200", "300")
     var value = ""
 
-    private val zero = Pair(0.0, 0.0)
-    private var vector: Array<Pair<Double, Double>> = arrayOf()
-    private var iterator = ArrayIterator<Pair<Double, Double>>()
-    private var indexedValueIterator = ArrayIndexedValueIterator(zero)
+    private var vector: Array<Double> = arrayOf()
+    private var iterator = ArrayIterator<Double>()
+    private var indexedValueIterator = ArrayIndexedValueIterator(0.0)
 
 
     @Setup(Level.Invocation)
     fun setUp() {
         val size = value.toInt()
         val rnd = ThreadLocalRandom.current()
-        vector = Array(size) { Pair(rnd.nextDouble(), rnd.nextDouble()) }
+        vector = Array(size) { rnd.nextDouble() }
         iterator = ArrayIterator(*vector)
-        indexedValueIterator = ArrayIndexedValueIterator(zero, *vector)
+        indexedValueIterator = ArrayIndexedValueIterator(0.0, *vector)
     }
-
-    fun getValue(pair: Pair<Double, Double>): Double = pair.first + pair.second
 
     @Benchmark
     fun testRangeLoop(): Double {
         var sum = 0.0
 
-        for (i in 0 until vector.size) {
-            sum += getValue(vector[i])
+        for (index in 0 until vector.size) {
+            if (index % 3 == 0) {
+                sum += vector[index]
+            }
         }
         return sum
     }
@@ -47,8 +45,10 @@ open class IteratorTest {
     fun testIterator(): Double {
         var sum = 0.0
 
-        for (value in iterator) {
-            sum += getValue(value)
+        for ((index, value) in iterator) {
+            if (index % 3 == 0) {
+                sum += value
+            }
         }
 
         return sum
@@ -59,11 +59,23 @@ open class IteratorTest {
         var sum = 0.0
 
         while (indexedValueIterator.hasNext()) {
-            indexedValueIterator.next { _, value ->
-                sum += getValue(value)
+            indexedValueIterator.next { index, value ->
+                if (index % 3 == 0) {
+                    sum += value
+                }
             }
         }
 
         return sum
     }
+}
+
+data class IndexedValue<T>(val index: Int, val value: T)
+
+class ArrayIterator<T>(vararg val values: T) : Iterator<IndexedValue<T>> {
+
+    var index = 0
+
+    override fun hasNext() = index < values.size
+    override fun next() = IndexedValue(index, values[index++])
 }
